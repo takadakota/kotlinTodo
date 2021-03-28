7efa2a19afaa366731a3d39924d229cf367f5ed3
takadakota <takadakota@team-lab.com>
2021-03-28 13:12:34 +0900

    update: Controller動作確認

--- src/main/kotlin/com/teamlab/skillup/todo/Controller/TodoController.kt
+++ src/main/kotlin/com/teamlab/skillup/todo/Controller/TodoController.kt
@@ -3,10 +3,17 @@ package com.teamlab.skillup.todo.Controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 
 @Controller
 class TodoController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     fun todo(model: Model): String = "todo"
+
+    @PutMapping("/add")
+    fun add(model: Model): String = "add"
+
+    @PutMapping("/edit")
+    fun edit(model: Model): String = "edit"
 }
