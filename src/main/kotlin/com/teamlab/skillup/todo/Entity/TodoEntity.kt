31bdb25d9058b95a34a8863f051838756d32c257
takadakota <takadakota@team-lab.com>
2021-03-30 18:35:45 +0900

    add: Entity雛形作成

---
+++ src/main/kotlin/com/teamlab/skillup/todo/Entity/TodoEntity.kt
@@ -0,0 +1,29 @@
+package com.teamlab.skillup.todo.Entity
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.persistence.*
+import org.seasar.doma.*;
+
+@Entity
+@Table(name = "テーブル名")
+data class TodoEntity (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Int,
+
+        @Column(name = "todoName")
+        val todoName: String,
+
+        @Column(name = "timeLimit")
+        val timeLimit: String,
+
+        @Column(name = "doneFlag")
+        val doneFlag: Boolean,
+
+        @Column(name = "createDate")
+        val createDate: LocalDate,
+
+        @Column(name = "updateTime")
+        val updateTime: LocalDateTime
+)
