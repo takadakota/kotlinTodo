a27b08125814b087cfad514f3c25425bc82801cf
takadakota <takadakota@team-lab.com>
2021-04-02 17:18:51 +0900

    update: Javadoc追加

--- src/main/kotlin/com/teamlab/skillup/todo/Controller/TodoController.kt
+++ src/main/kotlin/com/teamlab/skillup/todo/Controller/TodoController.kt
@@ -11,10 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/todo")
 class TodoController(private val service: TodoService) {
 
+    /**
+     * 初期表示API呼び出し
+     * String型を返してるのにmain/resource/template配下のindex.htmlが反応すのか…
+     * →その辺は優秀なフレームワークがよしなにやってくれてる
+     */
     @GetMapping("")
     fun todo(): String {
         return "index"
     }
+
+    /**
+     * Todo追加API呼び出し
+     * insert後、全件取得してHTMLに反映させる
+     */
     @PostMapping("")
     fun todoPost(todoName: String, timeLimit: String): String {
         service.insertAndAllTodoGet(todoName,timeLimit)
